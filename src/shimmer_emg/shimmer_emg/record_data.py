7d3b6d034d5c11765c3ed517d8fd36d6f4f16715
WilliamTolstrup <Wtolst19@student.aau.dk>
2024-05-03 13:54:26 +0200

    Updated to support new features

--- src/shimmer_emg/shimmer_emg/record_data.py
+++ src/shimmer_emg/shimmer_emg/record_data.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Bool, Float32
 from geometry_msgs.msg import Vector3
 import csv
 import os
@@ -10,45 +10,25 @@ import time
 class DataListener(Node):
     def __init__(self):
         super().__init__('data_listener')
-        self.features_filename = 'features_file.csv'
-        self.features_file_exists = os.path.isfile(self.features_filename)
-
-        self.continuous_filename = 'continuous_file.csv'
-        self.continuous_file_exists = os.path.isfile(self.continuous_filename)
+        self.data_filename = 'data_file.csv'
+        self.data_file_exists = os.path.isfile(self.data_filename)
 
         # Open the file in append mode and create a csv writer object
-        self.features_file = open(self.features_filename, mode='a', newline='')  # 'a' appends new data onto the existing file. 'w' wipes the file before writing.
-        self.continuous_file = open(self.continuous_filename, mode='a', newline='')
-        self.features_writer = csv.writer(self.features_file)
-        self.continuous_writer = csv.writer(self.continuous_file)
+        self.data_file = open(self.data_filename, mode='a', newline='')  # 'a' appends new data onto the existing file. 'w' wipes the file before writing.
+        self.data_writer = csv.writer(self.data_file)
 
         # Header for .csv file
-        if not self.features_file_exists:
-            self.features_writer.writerow([
-                                  'emg_raw_mav_ch1', 'emg_raw_mav_ch2', 
-                                  'emg_raw_rms_ch1', 'emg_raw_rms_ch2', 
-                                  'emg_raw_sd_ch1', 'emg_raw_sd_ch2', 
-                                  'emg_raw_wl_ch1', 'emg_raw_wl_ch2', 
-                                  'raw_coeff_1', 'raw_coeff_2', 'raw_coeff_3', 'raw_coeff_4', 'raw_coeff_5', 'raw_coeff_6', 'raw_coeff_7', 'raw_coeff_8', 
-
-                                  'emg_filtered_mav_ch1', 'emg_filtered_mav_ch2', 
-                                  'emg_filtered_rms_ch1', 'emg_filtered_rms_ch2', 
-                                  'emg_filtered_sd_ch1', 'emg_filtered_sd_ch2', 
-                                  'emg_filtered_wl_ch1', 'emg_filtered_wl_ch2', 
-                                  'filtered_coeff_1', 'filtered_coeff_2', 'filtered_coeff_3', 'filtered_coeff_4', 'filtered_coeff_5', 'filtered_coeff_6', 'filtered_coeff_7', 'filtered_coeff_8', 
-
-                                  'acc_mav_x', 'acc_mav_y', 'acc_mav_z', 
-                                  'acc_rms_x', 'acc_rms_y', 'acc_rms_z',
-                                  'acc_sd_x', 'acc_sd_y', 'acc_sd_z',
-     #                             'acc_diff_x', 'acc_diff_y', 'acc_diff_z',
-                                  'state'])
-
-        if not self.continuous_file_exists:
-            self.continuous_writer.writerow(['timestamp',
-                                             'emg_raw_ch1', 'emg_raw_ch2',
-                                             'emg_filtered_ch1', 'emg_filtered_ch2',
-                                             'ln_acc_x', 'ln_acc_y', 'ln_acc_z',
-                                             'state'])
+        if not self.data_file_exists:
+            self.data_writer.writerow(['timestamp',
+                                       'emg_raw_ch1', 'emg_raw_ch2',
+                                       'emg_filtered_ch1', 'emg_filtered_ch2',
+                                       'emg_rectified_ch1', 'emg_rectified_ch2',
+                                       'emg_envelope_ch1', 'emg_envelope_ch2'
+                                       'ln_acc_x', 'ln_acc_y', 'ln_acc_z',   
+                                       'ch1_contraction', 'ch2_contraction', 
+                                       'acc_y_derivative',
+                                       'elbow_angle',
+                                       'state'])
 
         self.iteration = 1
 
@@ -58,31 +38,50 @@ class DataListener(Node):
 
         # Initialize lists and flags
         self.timestamp_data = [0, 0]
-        self.emg_filtered_data  = [0, 0]
         self.emg_raw_data = [0, 0]
+        self.emg_filtered_data  = [0, 0]
+        self.emg_rectified_data = [0, 0]
+        self.emg_envelope_data = [0, 0]
         self.ln_acc_data  = [0, 0, 0]
-        self.features_data = []
+        self.ch1_contraction_data = [False]
+        self.ch2_contraction_data = [False]
+        self.derivative_data = [0]
+        self.angle_data = [0]
         self.current_state = 'rest'
         # Flags
         self.new_timestamp_data = False
         self.new_emg_raw_data = False
         self.new_emg_filtered_data = False
+        self.new_emg_rectified_data = False
+        self.new_emg_envelope_data = False
         self.new_ln_acc_data = False
-        self.new_features_data = False
-        self.timestamp_flag_one = False
-
+        self.new_ch1_contraction_data = False
+        self.new_ch2_contraction_data = False
+        self.new_derivative_data = False
+        self.new_angle_data = False
 
 
         # Timestamp subscriber
         self.timestamp_subscriber = self.create_subscription(Vector3, '/shimmer/timestamp', self.timestamp_callback, 10)
         # EMG raw data subscriber
         self.emg_raw_subscriber = self.create_subscription(Vector3, '/emg/emg_raw', self.emg_raw_data_callback, 10)
-        # EMG data subscriber
+        # EMG filtered data subscriber
         self.emg_filtered_subscriber = self.create_subscription(Vector3, '/emg/emg_filtered', self.emg_filtered_callback, 10)
+        # EMG rectified data subscriber
+        self.emg_rectified_subscriber = self.create_subscription(Vector3, '/emg/emg_rectified', self.emg_rectified_callback, 10)
+        # EMG envelope data subscriber
+        self.emg_envelope_subscriber = self.create_subscription(Vector3, '/emg/emg_envelope', self.emg_envelope_callback, 10)
         # IMU accelerometer data subscriber
         self.acc_subscriber = self.create_subscription(Vector3, '/imu/ln_acc', self.ln_acc_callback, 10)
-        # Features subscriber
-        self.features_subscriber = self.create_subscription(Float32MultiArray, '/shimmer/features', self.features_callback, 10)
+
+        # Contraction data subscriber
+        self.emg_ch1_contraction_subscriber = self.create_subscription(Bool, '/features/ch1_contractions', self.ch1_contraction_callback, 10)
+        self.emg_ch2_contraction_subscriber = self.create_subscription(Bool, '/features/ch2_contractions', self.ch2_contraction_callback, 10)
+        # Acc derivative data subscriber
+        self.acc_derivative_subscriber = self.create_subscription(Float32, '/features/AccDerivative', self.acc_derivative_callback, 10)
+        # Angle estimate
+        self.angle_estimate_subscriber = self.create_subscription(Float32, '/features/angle', self.angle_callback, 10)
+
 
         # Timing and protocol management
         self.start_time = time.time()
@@ -116,15 +115,41 @@ class DataListener(Node):
         self.new_emg_filtered_data = True
         self.log_and_save_data()
 
+    def emg_rectified_callback(self, msg):
+        self.emg_rectified_data = [msg.x, msg.y]
+        self.new_emg_rectified_data = True
+        self.log_and_save_data()
+
+    def emg_envelope_callback(self, msg):
+        self.emg_envelope_data = [msg.x, msg.y]
+        self.new_emg_envelope_data = True
+        self.log_and_save_data()
+
     def ln_acc_callback(self, msg):
         self.ln_acc_data = [msg.x, msg.y, msg.z]
         self.new_ln_acc_data = True
         self.log_and_save_data()
 
-    def features_callback(self, msg):
-        self.features_data = msg.data
-        self.new_features_data = True
-        self.log_and_save_features()
+    def ch1_contraction_callback(self, msg):
+        self.ch1_contraction_data = msg.data
+        self.new_ch1_contraction_data = True
+        self.log_and_save_data()
+
+    def ch2_contraction_callback(self, msg):
+        self.ch2_contraction_data = msg.data
+        self.new_ch2_contraction_data = True
+        self.log_and_save_data()
+
+    def acc_derivative_callback(self, msg):
+        self.derivative_data = msg.data
+        self.new_derivative_data = True
+        self.log_and_save_data()
+
+    def angle_callback(self, msg):
+        self.angle_data = msg.data
+        self.new_angle_data = True
+        self.log_and_save_data()
+
 
     def update_protocol_state(self):
         current_time = time.time()
@@ -144,41 +169,32 @@ class DataListener(Node):
                 if self.iteration == 51:
                     print("=========== STOP ===========")
 
-    def log_and_save_features(self):
-        if time.time() < self.delay_end_time:
-            return
-        
-        if self.new_features_data:
-            self.features_writer.writerow([self.features_data.tolist() + [self.current_state]])
-
-            self.new_features_data = False
-
-        else:
-            return
-
     def log_and_save_data(self):
         if time.time() < self.delay_end_time:
             return  # Skip logging and saving data during the delay period
         
         # Check if all data types have been updated
-        if self.new_timestamp_data and self.new_emg_raw_data and self.new_emg_filtered_data and self.new_ln_acc_data:
-            # Log data
-            #self.get_logger().info(f'Timestamp: {self.timestamp_data}, EMG_raw: {self.emg_raw_data}, EMG_filtered: {self.emg_filtered_data}, Acc: {self.acc_data}, Gyro: {self.gyro_data}, Mag: {self.mag_data}')
+        if self.new_timestamp_data and self.new_emg_raw_data and self.new_emg_filtered_data and self.new_emg_rectified_data and self.new_emg_envelope_data and self.new_ln_acc_data and self.new_ch1_contraction_data and self.new_ch2_contraction_data and self.new_derivative_data and self.new_angle_data:
             # Save data to csv
-            self.continuous_writer.writerow([self.timestamp_data] + self.emg_raw_data + self.emg_filtered_data + self.ln_acc_data + [self.current_state])
+            self.data_writer.writerow([self.timestamp_data] + self.emg_raw_data + self.emg_filtered_data + self.emg_rectified_data + self.emg_envelope_data + self.ln_acc_data + self.ch1_contraction_data + self.ch2_contraction_data + self.derivative_data + self.angle_data + [self.current_state])
 
             self.new_timestamp_data = False
             self.new_emg_raw_data = False
             self.new_emg_filtered_data = False
+            self.new_emg_rectified_data = False
+            self.new_emg_envelope_data = False
             self.new_ln_acc_data = False
+            self.new_ch1_contraction_data = False
+            self.new_ch2_contraction_data = False
+            self.new_derivative_data = False
+            self.new_angle_data = False
 
         else:
             return
         
     def __del__(self):
         # Close the file
-        self.features_file.close()
-        self.continuous_file.close()
+        self.data_file.close()
 
 
 def main(args=None):
