21dc55e3a2918c6efadf226ffe4ded4460e75b88
WilliamTolstrup <Wtolst19@student.aau.dk>
2024-05-03 14:07:37 +0200

    Updated to plot new features

--- src/shimmer_emg/shimmer_emg/plot_data.py
+++ src/shimmer_emg/shimmer_emg/plot_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load data from CSV
-data = pd.read_csv('continuous_file.csv')
+data = pd.read_csv('data_file.csv')
 
 # Determine unique states for coloring
 unique_states = data['state'].unique()
@@ -10,7 +10,7 @@ state_colors = {'rest_heavy_vertical': 'white', 'flexion_heavy_vertical': 'green
 
 
 # Plotting
-fig, axs = plt.subplots(3, 1, figsize=(10, 8), sharex=True)
+fig, axs = plt.subplots(8, 1, figsize=(10, 8), sharex=True)
 fig.suptitle('EMG and IMU Data Over Time')
 
 def plot_shaded_states(ax):
@@ -40,13 +40,46 @@ axs[1].set_ylabel('Filtered EMG')
 axs[1].legend()
 plot_shaded_states(axs[1])
 
-# Acc Data
-axs[2].plot(data['timestamp'].values, data['ln_acc_x'].values, label='Accel x')
-axs[2].plot(data['timestamp'].values, data['ln_acc_y'].values, label='Accel y')
-axs[2].plot(data['timestamp'].values, data['ln_acc_z'].values, label='Accel z')
-axs[2].set_ylabel('Accel')
+# EMG rectified Data
+axs[2].plot(data['timestamp'].values, data['emg_rectified_ch1'].values, label='EMG Rectified Ch1')
+axs[2].plot(data['timestamp'].values, data['emg_rectified_ch2'].values, label='EMG Rectified Ch2')
+axs[2].set_ylabel('Rectified EMG')
 axs[2].legend()
 plot_shaded_states(axs[2])
+
+# EMG envelope Data
+axs[3].plot(data['timestamp'].values, data['emg_envelope_ch1'].values, label='EMG Envelope Ch1')
+axs[3].plot(data['timestamp'].values, data['emg_envelope_ch2'].values, label='EMG Envelope Ch2')
+axs[3].set_ylabel('EMG Envelope')
+axs[3].legend()
+plot_shaded_states(axs[3])
+
+# Acc Data
+axs[4].plot(data['timestamp'].values, data['ln_acc_x'].values, label='Accel x')
+axs[4].plot(data['timestamp'].values, data['ln_acc_y'].values, label='Accel y')
+axs[4].plot(data['timestamp'].values, data['ln_acc_z'].values, label='Accel z')
+axs[4].set_ylabel('Accel')
+axs[4].legend()
+plot_shaded_states(axs[4])
+
+# EMG contraction Data
+axs[5].plot(data['timestamp'].values, data['ch1_contraction'].values, label='Ch1 contractions')
+axs[5].plot(data['timestamp'].values, data['ch2_contraction'].values, label='Ch2 contractions')
+axs[5].set_ylabel('EMG Contractions (Bool)')
+axs[5].legend()
+plot_shaded_states(axs[5])
+
+# Accel y derivative
+axs[6].plot(data['timestamp'].values, data['acc_y_derivative'].values, label='Accel Y derivative')
+axs[6].set_ylabel('Accel Derivative')
+axs[6].legend()
+plot_shaded_states(axs[6])
+
+# Elbow Angle
+axs[7].plot(data['timestamp'].values, data['elbow_angle'].values, label='Angle')
+axs[7].set_ylabel('Estimated Elbow Angle')
+axs[7].legend()
+plot_shaded_states(axs[7])
 
 # Set common labels
 for ax in axs:
